7c8aa31a974bc6338b4b56da428f70d6f2975929
david1111967 <david1111967@gmail.com>
2020-09-28 17:44:44 +0100

    Update Ej. 11 type numeric

--- ejercicios/11.php
+++ ejercicios/11.php
@@ -17,16 +17,21 @@
         <input type="text" value="" name="editorial">
         <br/>
         <label>páginas</label>
-        <input type="number" value="" name="paginas">
+        <input type="text" value="" name="paginas">
         <br/>
         <input type="submit" value="Enviar">
     </form>
     <?php
         echo "<hr>";
-        echo "Titulo: ", ($_POST["titulo"]), "<br/>";
-        echo "Autor: ", ($_POST["autor"]), "<br/>";
-        echo "Editorial: ", ($_POST["editorial"]), "<br/>";
-        echo "Paginas: ", ($_POST["paginas"]), "<br/>";
+        if (is_numeric($_POST["paginas"])){
+            echo "Titulo: ", ($_POST["titulo"]), "<br/>";
+            echo "Autor: ", ($_POST["autor"]), "<br/>";
+            echo "Editorial: ", ($_POST["editorial"]), "<br/>";
+            echo "Paginas: ", ($_POST["paginas"]), "<br/>";
+        }
+        else if (is_numeric($_POST["paginas"]) == false && empty($_POST["paginas"]) == false) {
+            echo "Introduce una página";
+        }
     ?>
 </body>
 </html>
